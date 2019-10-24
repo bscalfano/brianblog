e5ba810b0f26b351321c1683384d34ef6be4b0c4
Brian Scalfano <bscalfan@kent.edu>
2019-10-23 20:22:12 -0400

    I thought I pushed this????

--- start.bat
+++ start.bat
@@ -1,3 +1,8 @@
 set FLASK_APP=microblog.py
 set FLASK_DEBUG=1
+export MAIL_SERVER=smtp.googlemail.com
+export MAIL_PORT=587
+export MAIL_USE_TLS=1
+export MAIL_USERNAME="prof.cortana@gmail.com"
+export MAIL_PASSWORD="example.44281"
 flask run
