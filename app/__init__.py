25d114820ec615fd88fe552a408d4a63eef95f46
Brian Scalfano <bscalfan@kent.edu>
2019-11-18 20:53:15 -0500

    Up to date (11/18)

--- app/__init__.py
+++ app/__init__.py
@@ -8,16 +8,19 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
+mail = Mail(app)
 login.login_view = 'login'
 from app import routes, models, errors
 
 if not app.debug:
+    
 
     if app.config['MAIL_SERVER']:
         auth = None
@@ -45,3 +48,4 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
+    
